2d6f44b4e679c28790edc4e9c367772565011ef9
jgmorton <jmorton2222@gmail.com>
2015-11-07 17:04:30 -0800

    streamlined iterative update

--- src/util/iterUtil.lua
+++ src/util/iterUtil.lua
@@ -29,22 +29,18 @@ function iterUtil.initMultinomial()
 	return probs
 end
 
-local function findWeights(loader)
-
-	-- get next batch of inputs/targets
-    local x, y = loader:next_batch()
-
-    -- Initialize network
-    local current_state = init_state
+local function findWeights(input)
 
     -- Tensors to hold weight and target values
-    local w = torch.zeros(100, opt.batch_size, outputs)
-    local target = torch.zeros(100, opt.batch_size)
+    local w = torch.zeros(input:size(1), 100, outputs)
+
+    -- Initialize internal state of network
+    current_state = init_state
 
     -- forward pass
     for t=1,120 do
 
-        local lst = protos.rnn:forward{x[{{}, t}], unpack(current_state)}
+        local lst = protos.rnn:forward{input[{{}, t}], unpack(current_state)}
         current_state = {}
         for i=1,state_size do table.insert(current_state, lst[i]) end
 
@@ -52,18 +48,18 @@ local function findWeights(loader)
         	local weights = torch.exp(lst[#lst]) -- find weights
         	
         	--store values
-        	w[t - 20] = weights
-        	if opt.nbins > 0 then
-        		target[t - 20] = convert.toBins(y[{{}, t - 20}], opt.nbins)
-        	else
-        		target[t - 20] = y[{{}, t - 20}]
+        	w[{{}, t - 20}] = weights
+
+        	if (t - 20) % 25 == 0 then 
+        		print(string.format("%d percent done", t-20))
         	end
         end
     end
-    return w, target
+    return w
 end
 
-local function updateParams(ws, targets, total)
+-- Update parameters in Gaussian mixture
+local function updateParams(ws, targets)
 
 	-- Find new mus
 	local y_repeat = torch.repeatTensor(targets, opt.mixture_size, 1):transpose(1, 2)
@@ -71,158 +67,91 @@ local function updateParams(ws, targets, total)
 	local mu_new = torch.cdiv(mu_num, torch.sum(ws, 1))
 
 	-- Find new sigmas
-	local diff = y_repeat - torch.repeatTensor(mu_new, total, 1)
+	local diff = y_repeat - torch.repeatTensor(mu_new[{1, {}}], targets:size(1), 1)
 	local num = torch.sum(torch.cmul(ws, torch.pow(diff, 2)), 1)
-	local sigma_new = torch.sqrt(num/total)
+	local sigma_new = torch.sqrt(num/targets:size(1))
 	return mu_new[{1, {}}], sigma_new[{1, {}}]
 end
 
--- Function to update mean and std dev of distributions.
--- Can probably be done in larger batches to improve speed
-function iterUtil.updateNormal(loader)
-
-	-- Reset batch indices
-	loader.batch_ix = {1, 0}
-	loader.moreBatches = true
-
-	-- variable to keep track of tensor size
-	local total = 0
-
-	-- Initialize tensors
-	local ws = torch.Tensor(1e6, opt.mixture_size):zero()
-	local targets = torch.Tensor(1e6):zero()
-	local step = 100 * opt.batch_size
-
-	-- Initialize new tensors
-	local mu = torch.Tensor(opt.mixture_size)
-	local sigma = torch.Tensor(opt.mixture_size)
-
-	-- Initialize network
-    init_state = {}
-    for L=1,opt.num_layers do
-        local h_init = torch.zeros(opt.batch_size, opt.nn_size)
-        if opt.gpuid >=0 then h_init = h_init:cuda() end
-        table.insert(init_state, h_init:clone())
-        table.insert(init_state, h_init:clone())
-    end
-    state_size = #init_state
-
-	-- Iterate over all batches in all folds in training set
-	iterations = (opt.nfolds - 1) * loader.batches
-	for i = 1, iterations do
-
-		-- Find weights and target values, then reshape
-		w, target = findWeights(loader)
-		torch.reshape(w, step, opt.mixture_size)
-		torch.reshape(target, step)
-
-		-- Store values
-		ws[{{total + 1, total + step}, {}}] = w
-		targets[{{total + 1, total + step}}] = target
-		total = total + step
-
-		if i % 100 == 0 then 
-			print(string.format('%.1f percent complete', i/iterations*100))
-		end
-	end
-
-	-- Get rid of empty values
-	ws = ws[{{1, total}, {}}]
-	targets = targets[{{1, total}}]
-	collectgarbage()
-
-	mu_new, sigma_new = updateParams(ws, targets, total)
-
-	print('new mu: ')
-	print(mu_new)
-	print('new sigma: ')
-	print(sigma_new)
-
-	-- Define new criterion with updated distribution
-	protos.criterion = normalNLL(opt.mixture_size, mu_new, sigma_new)
-end
-
-local function updateProbs(ws, targets, total, nbins)
+-- Update probabilities in multinomial distribution
+local function updateProbs(ws, targets)
 	local new_probs = torch.zeros(ws:size(2), opt.nbins)
-	for i = 1, total do
+	for i = 1, ws:size(1) do
 		new_probs[{{}, targets[i]}] = new_probs[{{}, targets[i]}] + ws[i]
 	end
 	local sums = torch.sum(new_probs, 2)
 	return torch.cdiv(new_probs, torch.repeatTensor(sums[{{}, 1}], opt.nbins, 1):transpose(1, 2))
 end
 
--- Function to update binned probabilities of multinomial distributions.
+-- Function to take all inputs outside validation set and put them in one tensor
+local function getXY(loader)
+	local step = loader.X:size(2) * loader.X:size(3)
+	local input = torch.zeros((loader.nfolds - 1) * step, 120, 4)
+	local targets = torch.zeros((loader.nfolds - 1) * step, 100)
+	local count = 0
+	for i = 1, loader.nfolds do
+		if i ~= loader.valSet then
+			count = count + 1
+			local x = torch.reshape(loader.X[i], step, 120, 4)
+			local start = (count - 1) * step + 1
+			input[{{start, count * step}, {}, {}}] = x
+
+			local y = torch.reshape(loader.Y[i], step, 100)
+			targets[{{start, count * step}, {}}] = y
+		end
+	end
+	return input, targets
+end
+
+-- Function to update mean and std dev of distributions.
 -- Can probably be done in larger batches to improve speed
-function iterUtil.updateMultinomial(loader)
+function iterUtil.updateDist(loader)
 
-	-- Reset batch indices
-	loader.batch_ix = {1, 0}
-	loader.moreBatches = true
+	-- Get input tensor
+	local dim = (loader.nfolds - 1) * loader.X:size(2) * loader.X:size(3)
+	local input, targets = getXY(loader)
 
-	-- variable to keep track of tensor size
-	local total = 0
-
-	-- Initialize tensors
-	local ws = torch.Tensor(1e6, opt.multinomial_size):zero()
-	local targets = torch.Tensor(1e6):zero()
-	local step = 100 * opt.batch_size
+	-- Reshape and convert target tensor (if necessary)
+	targets = torch.reshape(targets, targets:size(1)*100)
+	if opt.nbins > 0 then
+        targets = convert.toBins(targets, opt.nbins)
+    end
+    collectgarbage()
 
 	-- Initialize network
     init_state = {}
     for L=1,opt.num_layers do
-        local h_init = torch.zeros(opt.batch_size, opt.nn_size)
+        local h_init = torch.zeros(dim, opt.nn_size)
         if opt.gpuid >=0 then h_init = h_init:cuda() end
         table.insert(init_state, h_init:clone())
         table.insert(init_state, h_init:clone())
     end
     state_size = #init_state
 
-	-- Iterate over all batches in all folds in training set
-	iterations = (opt.nfolds - 1) * loader.batches
-	for i = 1, iterations do
-
-		-- Find weights and target values, then reshape
-		w, target = findWeights(loader, opt.nbins)
-		torch.reshape(w, step, opt.multinomial_size)
-		torch.reshape(target, step)
-
-		-- Store values
-		ws[{{total + 1, total + step}, {}}] = w
-		targets[{{total + 1, total + step}}] = target
-		total = total + step
-
-		if i % 100 == 0 then 
-			print(string.format('%.1f percent complete', i/iterations*100))
-		end
-	end
-
-	-- Get rid of empty values
-	ws = ws[{{1, total}, {}}]
-	targets = targets[{{1, total}}]
+	-- Find weights and target values, then reshape
+	ws = findWeights(input)
+	ws = torch.reshape(ws, dim * 100, opt.mixture_size + opt.multinomial_size)
 	collectgarbage()
 
-	probs_new = updateProbs(ws, targets, total)
+	-- Update multinomial distribution
+	if opt.nbins > 0 then
+		probs_new = updateProbs(ws, targets)
+		print('new probs: ')
+		print(probs_new)
+		-- Define new criterion with updated distribution
+		protos.criterion = iterativeNLL(opt.nbins, probs_new)
 
-	print('new probs: ')
-	print(probs_new)
-
-	-- Define new criterion with updated distribution
-	protos.criterion = iterativeNLL(opt.nbins, probs_new)
+	-- Update Gaussian mixture parameters
+	else
+		mu_new, sigma_new = updateParams(ws, targets)
+		print('new mu: ')
+		print(mu_new)
+		print('new sigma: ')
+		print(sigma_new)
+		-- Define new criterion with updated distribution
+		protos.criterion = normalNLL(opt.mixture_size, mu_new, sigma_new)
+	end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 return iterUtil
